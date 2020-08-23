aebb8b38c50a0a004cd990cdcd2e40fcf5eb95dc
Simon Braillard <simon.braillard@gmail.com>
2020-08-23 17:39:56 +0200

    formatting

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## [0.0.2] - 23 August 2020
+
+* Formatting dart files !
+
 ## [0.0.1] - 23 August 2020
 
 * First release !
--- example/main.dart
+++ example/main.dart
@@ -38,35 +38,74 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             /// You can use the other functions to test if you're on big or small tablet, or just tablet or on desktop
-            ResponsiveSize.isMobile(context) ? Text("We're on mobile") : Text("We're not on mobile"),
+            ResponsiveSize.isMobile(context)
+                ? Text("We're on mobile")
+                : Text("We're not on mobile"),
 
             /// Here we use the function getResponsiveSize to change the size of the text depending on the device
-            Text("This text has a responsive font size !", style: TextStyle(fontSize: ResponsiveSize.getResponsiveSize(context, 15))),
+            Text("This text has a responsive font size !",
+                style: TextStyle(
+                    fontSize: ResponsiveSize.getResponsiveSize(context, 15))),
 
             /// Here is the same as above, but the scale factor will be lower
-            Text("This text has a smaller responsive font size !", style: TextStyle(fontSize: ResponsiveSize.getSmallerResponsiveSize(context, 15))),
+            Text("This text has a smaller responsive font size !",
+                style: TextStyle(
+                    fontSize:
+                        ResponsiveSize.getSmallerResponsiveSize(context, 15))),
             Container(
               height: ResponsiveSize.getResponsiveSize(context, 500),
               child: GridView.count(
                 crossAxisSpacing: ResponsiveSize.getResponsiveSize(context, 10),
-                mainAxisSpacing: ResponsiveSize.getSmallerResponsiveSize(context, 10),
+                mainAxisSpacing:
+                    ResponsiveSize.getSmallerResponsiveSize(context, 10),
 
                 /// Here we use the responsive package to get a responsive cross axis column numbers
                 /// We can also specify the starter amount (2 by default on mobile) using the "startColCount" parameter
                 /// to get more columns if needed.
-                crossAxisCount: ResponsiveSize.responsiveGridColumnCount(context),
+                crossAxisCount:
+                    ResponsiveSize.responsiveGridColumnCount(context),
                 children: [
-                  Container(color: Colors.grey, child: Center(child: Text("This grid view has a responsive crossAxisCount !"),),),
-                  Container(color: Colors.grey, child: Center(child: Text("This grid view has a responsive crossAxisCount !"),),),
-                  Container(color: Colors.grey, child: Center(child: Text("This grid view has a responsive crossAxisCount !"),),),
-                  Container(color: Colors.grey, child: Center(child: Text("This grid view has a responsive crossAxisCount !"),),),
-                  Container(color: Colors.grey, child: Center(child: Text("This grid view has a responsive crossAxisCount !"),),),
+                  Container(
+                    color: Colors.grey,
+                    child: Center(
+                      child: Text(
+                          "This grid view has a responsive crossAxisCount !"),
+                    ),
+                  ),
+                  Container(
+                    color: Colors.grey,
+                    child: Center(
+                      child: Text(
+                          "This grid view has a responsive crossAxisCount !"),
+                    ),
+                  ),
+                  Container(
+                    color: Colors.grey,
+                    child: Center(
+                      child: Text(
+                          "This grid view has a responsive crossAxisCount !"),
+                    ),
+                  ),
+                  Container(
+                    color: Colors.grey,
+                    child: Center(
+                      child: Text(
+                          "This grid view has a responsive crossAxisCount !"),
+                    ),
+                  ),
+                  Container(
+                    color: Colors.grey,
+                    child: Center(
+                      child: Text(
+                          "This grid view has a responsive crossAxisCount !"),
+                    ),
+                  ),
                 ],
               ),
             )
           ],
         ),
-      ),// This trailing comma makes auto-formatting nicer for build methods.
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
