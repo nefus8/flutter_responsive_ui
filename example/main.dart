8fe02c0ac62ef4ffc331f8d26434c1f65da439a4
Simon Braillard <simon.braillard@gmail.com>
2020-08-23 17:35:36 +0200

    Updating readme

--- example/main.dart
+++ example/main.dart
@@ -1,23 +1,3 @@
-# Flutter responsive UI
-
-This flutter package allows you to resize font responsively and get to know what kind of device the app is running on.
-
-## Getting Started
-
-To use this package, add `flutter_responsive_ui` as a [dependency in your pubspec.yaml file](https://flutter.io/platform-plugins/). 
-
-You can use this package in multiple cases. You can get the type of the device that you're running your application on ! You can find if it's a **mobile**, a **tablet** or a **desktop** ! You can even make the deference between a **small tablet** and a **large tablet** !
-
-You can also make your UI and font size responsive depending on the device you're running the app on. By default, you'll need to develop on a **mobile emulator** because the default scale factor are based on small screens. If you want to directly develop on **tablet or desktop emulators**, feel free to use the ```init``` function to set the scale factors you want.
-
-## Functions
-You can easily know on what device you're on using : ``` isMobile(), isTablet(), isSmallTablet(), isLargeTablet() and isDesktop() ```.
-
-To get a responsive double, use ```getResponsiveSize()``` and to get a smaller double, use ```getSmallerResponsiveSize()```. And to get the amount of column on a GridView, you can use ```responsiveGridColumnCount()```.
-
-## Example
-
-```dart
 import 'package:flutter/material.dart';
 import 'package:flutter_responsive_ui/flutter_responsive_ui.dart';
 
@@ -90,4 +70,3 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-```
--- lib/flutter_responsive_ui.dart
+++ lib/flutter_responsive_ui.dart
@@ -3,21 +3,26 @@ library flutter_responsive_ui;
 import 'package:flutter/material.dart';
 
 class ResponsiveSize {
+  /// Here are the default values for the mobile size limit
+  /// It means the limit amount of points where we stop considering it as a mobile, small tablet etc...
   static double _mobileSizeLimit = 450;
   static double _smallTabletSizeLimit = 850;
   static double _largeTabletSizeLimit = 1200;
 
+  /// Here are the default values for the normal scale factor
   static double _mobileScaleFactor = 1.0;
   static double _smallTabletScaleFactor = 1.4;
   static double _largeTabletScaleFactor = 1.7;
   static double _desktopScaleFactor = 2;
 
+  /// Here are the default values for the smaller scale factor
   static double _mobileSmallerScaleFactor = 1.0;
   static double _smallTabletSmallerScaleFactor = 1.15;
   static double _largeTabletSmallerScaleFactor = 1.4;
   static double _desktopSmallerScaleFactor = 1.65;
 
   static void initValues({
+    /// All values can be set and they don't NEED to be set
     double mobileSizeLimit = 450,
     double smallTabletSizeLimit = 850,
     double largeTabletSizeLimit = 1200,
@@ -30,6 +35,8 @@ class ResponsiveSize {
     double largeTabletSmallerScaleFactor = 1.4,
     double desktopSmallerScaleFactor = 1.65,
   }) {
+
+    /// The initValues function is used to set the default values when the app stars
     _mobileSizeLimit = mobileSizeLimit;
     _smallTabletSizeLimit = smallTabletSizeLimit;
     _largeTabletSizeLimit = largeTabletSizeLimit;
@@ -45,29 +52,35 @@ class ResponsiveSize {
     _desktopSmallerScaleFactor = desktopSmallerScaleFactor;
   }
 
+  /// The function that return whether or not it's a mobile
   static bool isMobile(BuildContext context) {
     return MediaQuery.of(context).size.shortestSide < _mobileSizeLimit;
   }
 
+  /// The function that return whether or not it's a tablet (small and large)
   static bool isTablet(BuildContext context) {
     double shortestSide = MediaQuery.of(context).size.shortestSide;
     return (shortestSide >= _mobileSizeLimit && shortestSide < _largeTabletSizeLimit);
   }
 
+  /// The function that return whether or not it's a small tablet
   static bool isSmallTablet(BuildContext context) {
     double shortestSide = MediaQuery.of(context).size.shortestSide;
     return (shortestSide >= _mobileSizeLimit && shortestSide < _smallTabletSizeLimit);
   }
 
+  /// The function that return whether or not it's a large tablet
   static bool isLargeTablet(BuildContext context) {
     double shortestSide = MediaQuery.of(context).size.shortestSide;
     return (shortestSide >= _smallTabletSizeLimit && shortestSide < _largeTabletSizeLimit);
   }
 
+  /// The function that return whether or not it's a desktop
   static bool isDesktop(BuildContext context) {
     return (MediaQuery.of(context).size.shortestSide >= _largeTabletSizeLimit);
   }
-
+  
+  /// The function that return the responsive size
   static double getResponsiveSize(BuildContext context, double size) {
     double shortestSideSize = MediaQuery.of(context).size.shortestSide;
     if (shortestSideSize < _mobileSizeLimit) {
@@ -81,6 +94,7 @@ class ResponsiveSize {
     }
   }
 
+  /// The function that return the smaller responsive size
   static double getSmallerResponsiveSize(BuildContext context, double size) {
     double shortestSideSize = MediaQuery.of(context).size.shortestSide;
     if (shortestSideSize < _mobileSizeLimit) {
@@ -94,7 +108,8 @@ class ResponsiveSize {
     }
   }
 
-  static int responsiveGridItemCount(BuildContext context, {int startColCount = 2}) {
+  /// The function that return the responsive grid item. You can choose the starting column count.
+  static int responsiveGridColumnCount(BuildContext context, {int startColCount = 2}) {
     double shortestSideSize = MediaQuery.of(context).size.shortestSide;
     if (shortestSideSize < _mobileSizeLimit) {
       return startColCount;
