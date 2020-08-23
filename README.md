b7c6df1be82c1c538706112c218828d757f7d348
Simon Braillard <simon.braillard@gmail.com>
2020-08-23 17:10:52 +0200

    Responsive UI

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-# flutter_responsive_ui
+# Flutter responsive UI
 
 This flutter package allows you to resize font responsively and get to know what kind of device the app is running on.
 
 ## Getting Started
 
-This project is a starting point for a Dart
-[package](https://flutter.dev/developing-packages/),
-a library module containing code that can be shared easily across
-multiple Flutter or Dart projects.
+To use this package, add `flutter_responsive_ui` as a [dependency in your pubspec.yaml file](https://flutter.io/platform-plugins/). 
 
-For help getting started with Flutter, view our 
-[online documentation](https://flutter.dev/docs), which offers tutorials, 
-samples, guidance on mobile development, and a full API reference.
+You can use this package in multiple cases. You can get the type of the device that you're running your application on ! You can find if it's a **mobile**, a **tablet** or a **desktop** ! You can even make the deference between a **small tablet** and a **large tablet** !
+
+You can also make your UI and font size responsive depending on the device you're running the app on. By default, you'll need to develop on a **mobile emulator** because the default scale factor are based on small screens. If you want to directly develop on **tablet or desktop emulators**, feel free to use the ```init``` function to set the scale factors you want.
+
+## Functions
+You can easily know on what device you're on using : ```  ```
--- lib/flutter_responsive_ui.dart
+++ lib/flutter_responsive_ui.dart
@@ -1,7 +1,109 @@
 library flutter_responsive_ui;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class ResponsiveSize {
+  static double _mobileSizeLimit = 450;
+  static double _smallTabletSizeLimit = 850;
+  static double _largeTabletSizeLimit = 1200;
+
+  static double _mobileScaleFactor = 1.0;
+  static double _smallTabletScaleFactor = 1.4;
+  static double _largeTabletScaleFactor = 1.7;
+  static double _desktopScaleFactor = 2;
+
+  static double _mobileSmallerScaleFactor = 1.0;
+  static double _smallTabletSmallerScaleFactor = 1.15;
+  static double _largeTabletSmallerScaleFactor = 1.4;
+  static double _desktopSmallerScaleFactor = 1.65;
+
+  static void initValues({
+    double mobileSizeLimit = 450,
+    double smallTabletSizeLimit = 850,
+    double largeTabletSizeLimit = 1200,
+    double mobileScaleFactor = 1.0,
+    double smallTabletScaleFactor = 1.4,
+    double largeTabletScaleFactor = 1.7,
+    double desktopScaleFactor = 2,
+    double mobileSmallerScaleFactor = 1.0,
+    double smallTabletSmallerScaleFactor = 1.15,
+    double largeTabletSmallerScaleFactor = 1.4,
+    double desktopSmallerScaleFactor = 1.65,
+  }) {
+    _mobileSizeLimit = mobileSizeLimit;
+    _smallTabletSizeLimit = smallTabletSizeLimit;
+    _largeTabletSizeLimit = largeTabletSizeLimit;
+
+    _mobileScaleFactor = mobileScaleFactor;
+    _smallTabletScaleFactor = smallTabletScaleFactor;
+    _largeTabletScaleFactor = largeTabletScaleFactor;
+    _desktopScaleFactor = desktopScaleFactor;
+
+    _mobileSmallerScaleFactor = mobileSmallerScaleFactor;
+    _smallTabletSmallerScaleFactor = smallTabletSmallerScaleFactor;
+    _largeTabletSmallerScaleFactor = largeTabletSmallerScaleFactor;
+    _desktopSmallerScaleFactor = desktopSmallerScaleFactor;
+  }
+
+  static bool isMobile(BuildContext context) {
+    return MediaQuery.of(context).size.shortestSide < _mobileSizeLimit;
+  }
+
+  static bool isTablet(BuildContext context) {
+    double shortestSide = MediaQuery.of(context).size.shortestSide;
+    return (shortestSide >= _mobileSizeLimit && shortestSide < _largeTabletSizeLimit);
+  }
+
+  static bool isSmallTablet(BuildContext context) {
+    double shortestSide = MediaQuery.of(context).size.shortestSide;
+    return (shortestSide >= _mobileSizeLimit && shortestSide < _smallTabletSizeLimit);
+  }
+
+  static bool isLargeTablet(BuildContext context) {
+    double shortestSide = MediaQuery.of(context).size.shortestSide;
+    return (shortestSide >= _smallTabletSizeLimit && shortestSide < _largeTabletSizeLimit);
+  }
+
+  static bool isDesktop(BuildContext context) {
+    return (MediaQuery.of(context).size.shortestSide >= _largeTabletSizeLimit);
+  }
+
+  static double getResponsiveSize(BuildContext context, double size) {
+    double shortestSideSize = MediaQuery.of(context).size.shortestSide;
+    if (shortestSideSize < _mobileSizeLimit) {
+      return size*_mobileScaleFactor;
+    } else if (shortestSideSize >= _mobileSizeLimit && shortestSideSize < _smallTabletSizeLimit) {
+      return size*_smallTabletScaleFactor;
+    } else if (shortestSideSize >= _smallTabletSizeLimit && shortestSideSize < _largeTabletSizeLimit) {
+      return size*_largeTabletScaleFactor;
+    } else {
+      return size*_desktopScaleFactor;
+    }
+  }
+
+  static double getSmallerResponsiveSize(BuildContext context, double size) {
+    double shortestSideSize = MediaQuery.of(context).size.shortestSide;
+    if (shortestSideSize < _mobileSizeLimit) {
+      return size*_mobileSmallerScaleFactor;
+    } else if (shortestSideSize >= _mobileSizeLimit && shortestSideSize < _smallTabletSizeLimit) {
+      return size*_smallTabletSmallerScaleFactor;
+    } else if (shortestSideSize >= _smallTabletSizeLimit && shortestSideSize < _largeTabletSizeLimit) {
+      return size*_largeTabletSmallerScaleFactor;
+    } else {
+      return size*_desktopSmallerScaleFactor;
+    }
+  }
+
+  static int responsiveGridItemCount(BuildContext context, {int startColCount = 2}) {
+    double shortestSideSize = MediaQuery.of(context).size.shortestSide;
+    if (shortestSideSize < _mobileSizeLimit) {
+      return startColCount;
+    } else if (shortestSideSize >= _mobileSizeLimit && shortestSideSize < _smallTabletSizeLimit) {
+      return startColCount+1;
+    } else if (shortestSideSize >= _smallTabletSizeLimit && shortestSideSize < _largeTabletSizeLimit) {
+      return startColCount+2;
+    } else {
+      return startColCount+3;
+    }
+  }
 }
